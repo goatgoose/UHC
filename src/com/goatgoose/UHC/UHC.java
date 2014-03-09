64a6fe1c36dc5084bf306fd4a64ecad1a296dd54
Sam Clark <sam.clark1142@gmail.com>
2014-03-08 22:27:45 -0500

    template

---
+++ src/com/goatgoose/UHC/UHC.java
@@ -0,0 +1,17 @@
+package com.goatgoose.uhc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UHC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
